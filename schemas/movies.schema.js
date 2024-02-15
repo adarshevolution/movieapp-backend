6c714a452a89e37bfcc801b16add7e41bce0b6f9
adarshevolution <adarsh.b@evolutioncloud.in>
2024-02-15 16:33:18 +0530

    get, post, put movies api create

--- schemas/movies.schema.js
+++ schemas/movies.schema.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
 
-const MoviesSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please enter the movie name"],
+const MoviesSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter the movie name"],
+    },
+    publishYear: {
+      type: Number,
+      required: [true, "please provide a valid publish year"],
+    },
+    image: {
+      type: String,
+      required: [true, "Please  upload an Image for this Movie"],
+    },
   },
-  publiseYear: {
-    type: Number,
-    required: [true, "please provide a valid year"],
-  },
-  image: {
-    type: String,
-    required: [true, "Please  upload an Image for this Movie"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 const movies = mongoose.model("movies", MoviesSchema);
 
 module.exports = movies;
